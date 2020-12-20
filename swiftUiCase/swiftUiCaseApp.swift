802351a9ee746bec0f35e91775d7732c3a89381b
9haomi <>
2020-12-20 21:39:28 +0800

    项目初始化

---
+++ swiftUiCase/swiftUiCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUiCaseApp.swift
+//  swiftUiCase
+//
+//  Created by 9haomi on 2020/12/20.
+//
+
+import SwiftUI
+
+@main
+struct swiftUiCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftUiCaseTests/swiftUiCaseTests.swift
@@ -0,0 +1,33 @@
+//
+//  swiftUiCaseTests.swift
+//  swiftUiCaseTests
+//
+//  Created by 9haomi on 2020/12/20.
+//
+
+import XCTest
+@testable import swiftUiCase
+
+class swiftUiCaseTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
