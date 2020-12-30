9576e48ed10fa343d61440c0ced2bc202f724d71
Jiabingbing <jiabingbing@adesk.com>
2020-12-30 10:34:14 +0800

    充电状态

--- SwiftUIBatteryState/ContentView.swift
+++ SwiftUIBatteryState/ContentView.swift
@@ -8,11 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var batteryState = "未知"
+    
     init() {
         
         UIDevice.current.isBatteryMonitoringEnabled = true;
         
         print("当前电量0是："+String(UIDevice.current.batteryLevel))
+        
+        print("当前充电状态："+String(UIDevice.current.batteryState.rawValue))
+        
+        if UIDevice.current.batteryState.rawValue == UIDevice.BatteryState.unknown.rawValue {
+            //未知
+            batteryState = "未知"
+        }else if  UIDevice.current.batteryState.rawValue == UIDevice.BatteryState.unplugged.rawValue{
+            //放电
+            batteryState = "放电"
+        }else if  UIDevice.current.batteryState.rawValue == UIDevice.BatteryState.charging.rawValue{
+            //充电
+            batteryState = "充电"
+        }else if  UIDevice.current.batteryState.rawValue == UIDevice.BatteryState.full.rawValue{
+            //充满
+            batteryState = "充满"
+        }
 
         NotificationCenter.default.addObserver(forName: UIDevice.batteryStateDidChangeNotification, object: nil, queue: nil) { (Notification) in
             print("11111111111")
@@ -26,7 +45,7 @@ struct ContentView: View {
         
     }
     var body: some View {
-        Text("Hello, world!")
+        Text(batteryState)
             .padding()
     }
 }
